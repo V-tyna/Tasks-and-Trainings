460e61a65cb5de48f72ab6b00dacc885df9c2f1f
Valentyna Boiko <boiko.vtyna@gmail.com>
2022-04-30 15:03:45 +0200

    Add unique digits task

--- differentTasks/index.js
+++ differentTasks/index.js
@@ -14,3 +14,26 @@ function removeZeroAndDash(num) {
 
 removeZeroAndDash(-909010090);
 
+// #1 way Unique digits
+
+const ununiqueArr = [1, 2, 5, 1, 7, 3, 2, 9];
+
+const uniqueDigits = (arr) => {
+    return Array.from(new Set(arr));
+}
+
+console.log('First way: ', uniqueDigits(ununiqueArr));
+
+// #2 way Unique digits
+
+const uniqueDigitsLoops = (arr) => {
+    const finalArr = [];
+    for(let i = 0 ; i < arr.length; i++) {
+        if(!finalArr.includes(arr[i])) {
+            finalArr.push(arr[i]);
+        } 
+    }
+    return finalArr;
+}
+
+console.log('Second way: ', uniqueDigitsLoops(ununiqueArr));
