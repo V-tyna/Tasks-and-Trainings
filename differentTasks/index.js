c405e9f4c497608abc5e2f0caab5fdccd977b08c
Валентина Бойко <boiko.vtyna@gmail.com>
2022-10-17 11:53:31 +0300

    Multiply All (Simple curring).

--- differentTasks/index.js
+++ differentTasks/index.js
@@ -1844,3 +1844,17 @@ function stat(strg) {
 console.log('Statistic: ', stat('01|15|59, 1|47|16, 01|17|20, 1|32|34, 2|17|17')); // 'Range: 01|01|18 Average: 01|38|05 Median: 01|32|34')
 console.log('Statistic: ', stat('02|15|59, 2|47|16, 02|17|20, 2|32|34, 2|17|17, 2|22|00, 2|31|41')); // 'Range: 00|31|17 Average: 02|26|18 Median: 02|22|00'
 console.log('Statistic: ', stat('02|15|59, 2|47|16, 02|17|20, 2|32|34, 2|32|34, 2|17|17')); // 'Range: 00|31|17 Average: 02|27|10 Median: 02|24|57'
+
+//---------------------------------------------------------------------------------------------------------------------------
+//                                   Multiply All (Curring)
+//---------------------------------------------------------------------------------------------------------------------------
+
+// function multiplyAll(arr) {
+// 	return function(n) {
+// 		return arr.map(el => el * n);
+// 	}
+// }
+
+const multiplyAll = (arr) => (n) => arr.map(x => x * n);
+
+console.log('Curring multiple all: ', multiplyAll([1, 2, 3])(2)); // [2, 4, 6]
